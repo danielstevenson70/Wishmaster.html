aaa842dc2366d67d22472c3ad3e779c2215a62fb
Daniel Stevenson <danielstevenson70@gmail.com>
2025-05-19 16:16:17 -0400

    Update scripts.js

--- Wishmaster.html/scripts.js
+++ Wishmaster.html/scripts.js
@@ -1,2 +1,46 @@
 'use strict';
 
+ let wishCount = 0;
+ let maxWishes = 3;
+
+
+function handleWish(event) {
+    event.preventDefault();
+    const wishInput = document.getElementById('wishInput');
+    const wishItem = document.getElementById('wish');
+    const wishText = wishInput.value();
+    const wishList = document.getElementById('wishList');
+    const wishCount = document.getElementById('wishCount');
+    const response = document.getElementById('response');
+
+function grantWish() {
+    if (wishCount <= maxWishes) {
+        wishCount++;
+        document.getElementById('wishCount').textContent = 'Wish Granted: ' + wishCount;
+    }   else {
+        alert("Number of wishes have been fulfilled")
+    } 
+        
+    }
+
+    if (!wish) return;
+
+    if (wishcount === maxWish) {
+        showpopup();
+    }
+}
+
+    if (wishCount >= maxWishes) {
+        showPopup();
+    }
+
+
+function showPopup() {
+    document.getElementById('geniePopup').classList.add('show');
+}
+
+function hidePopup() {
+    document.getElementById('geniePopup').classList.remove('show');
+}
+
+
